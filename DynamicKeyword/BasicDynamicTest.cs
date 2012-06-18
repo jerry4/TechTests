dabd56ed61972bcbfa98f65c4982a57f667b255a
jerry4 <jerry4@gmail.com>
2012-06-17 23:15:53 -0400

    Some tests for rp test

--- DynamicKeyword/BasicDynamicTest.cs
+++ DynamicKeyword/BasicDynamicTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using Microsoft.CSharp.RuntimeBinder;
 using Xunit;
@@ -60,6 +61,25 @@ namespace DynamicKeyword
       // not great rhs has to be a specific type of class that derives from DynamicObject
       dynamic a = new CustomDynamic();
       Assert.DoesNotThrow(() => a.Test() );
+    }
+
+    [Fact]
+    public void DictionaryWithDynamicValue()
+    {
+      var dict = new Dictionary<string, dynamic>();
+      dict["test"] = new Url("http://google.com");
+      Url storedDynamic = dict["test"];
+      Assert.Equal("http://google.com", storedDynamic.Value);
+    }
+
+    [Fact]
+    public void ExpandoObjectForTests()
+    {
+      dynamic dict = new ExpandoObject(); // var not good enough. must be dynamic
+      dict.test = 1;
+      // some time later
+      int actual = dict.test;
+      Assert.Equal(1, actual);
     }
   }
 }
